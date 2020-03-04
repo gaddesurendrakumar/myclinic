b6e2b3439b5b9b63afcc62f5110f9b45393435ce
sgadde <gaddesurendrakumar@gmail.com>
2020-03-04 11:11:14 +0530

    Initial Project addition

---
+++ src/main/java/my/clinic/myclinic/MyclinicApplication.java
@@ -0,0 +1,13 @@
+package my.clinic.myclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/clinic/myclinic/MyclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package my.clinic.myclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
