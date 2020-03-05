3b6a02cd5b05e6b7f718826e359207ebc7be6c58
sgadde <gaddesurendrakumar@gmail.com>
2020-03-05 23:56:32 +0530

    Created Vet Index Page and Controllers, closes #7

---
+++ my-clinic-web/src/main/java/my/clinic/myclinic/controllers/VetsController.java
@@ -0,0 +1,13 @@
+package my.clinic.myclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetsController {
+
+    @RequestMapping({"/vets","/vets/index","vets/index.html"})
+    public String listVets(){
+        return "vets/index";
+    }
+}
