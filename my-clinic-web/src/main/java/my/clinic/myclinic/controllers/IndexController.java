1503cf589e90f702f404887cc57a9afe0cc687c1
sgadde <gaddesurendrakumar@gmail.com>
2020-03-05 22:16:19 +0530

    Add my clinic index page and controller, closes #8

---
+++ my-clinic-web/src/main/java/my/clinic/myclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package my.clinic.myclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"","/","index","index.html"})
+    public String index(Model model){
+        return  "index";
+    }
+}
