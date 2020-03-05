c4dbe461ebf76763c6b7cf22e978634467ae5072
sgadde <gaddesurendrakumar@gmail.com>
2020-03-05 16:57:50 +0530

    Refactored service interfaces to common base interface, closes #11

--- my-clinic-data/src/main/java/my/clinic/myclinic/services/CrudService.java
+++ my-clinic-data/src/main/java/my/clinic/myclinic/services/CrudService.java
@@ -3,9 +3,11 @@ package my.clinic.myclinic.services;
 import java.util.Set;
 
 public interface CrudService<T,ID> {
+    //common interface
     T findById(ID id);
     T save(T object);
     Set<T> findAll();
     void delete(T object);
     void deleteById(ID id);
+
 }
