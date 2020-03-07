848ea2ff7772281d8d3ecf2cc2984281516b2440
sgadde <gaddesurendrakumar@gmail.com>
2020-03-08 01:35:26 +0530

    Create Speciality Map Service, closes #31

---
+++ my-clinic-data/src/main/java/my/clinic/myclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package my.clinic.myclinic.services;
+
+import my.clinic.myclinic.domain.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty,Long> {
+}
---
+++ my-clinic-data/src/main/java/my/clinic/myclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,34 @@
+package my.clinic.myclinic.services.map;
+
+import my.clinic.myclinic.domain.Specialty;
+import my.clinic.myclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+}
