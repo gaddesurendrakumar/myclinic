8bff669ab4ca7e81c3075c82cb1709d51090ce8d
sgadde <gaddesurendrakumar@gmail.com>
2020-03-07 12:13:30 +0530

    Listed all Owners on Owners Index page,closes #16

--- my-clinic-web/src/main/java/my/clinic/myclinic/controllers/VetsController.java
+++ my-clinic-web/src/main/java/my/clinic/myclinic/controllers/VetsController.java
@@ -1,13 +1,21 @@
 package my.clinic.myclinic.controllers;
 
+import my.clinic.myclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
+@RequestMapping("/vets")
+@Controller()
 public class VetsController {
+private final VetService vetService;
 
-    @RequestMapping({"/vets","/vets/index","vets/index.html"})
-    public String listVets(){
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"","/index","/index.html"})
+    public String listVets(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
