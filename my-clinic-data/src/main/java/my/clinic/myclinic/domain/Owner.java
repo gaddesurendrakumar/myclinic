7944ef424dc40444aafdcfba8984df802026e79a
sgadde <gaddesurendrakumar@gmail.com>
2020-03-08 00:33:04 +0530

    Created Pet Type, Pet and Visit Entities closes #26

--- my-clinic-data/src/main/java/my/clinic/myclinic/domain/Owner.java
+++ my-clinic-data/src/main/java/my/clinic/myclinic/domain/Owner.java
@@ -1,10 +1,22 @@
 package my.clinic.myclinic.domain;
 
+import java.util.Set;
+
 public class Owner extends  Person {
 
     private String address;
     private String city;
     private String telephone;
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 
     public String getAddress() {
         return address;
