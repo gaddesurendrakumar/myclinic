49e8b1c595573002f95a32cafbe923adde364b8c
sgadde <gaddesurendrakumar@gmail.com>
2020-03-09 15:51:23 +0530

    Create Visit Map Service, closes #46

---
+++ my-clinic-data/src/main/java/my/clinic/myclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package my.clinic.myclinic.services;
+
+import my.clinic.myclinic.domain.Visit;
+
+public interface VisitService extends CrudService<Visit,Long> {
+}
---
+++ my-clinic-data/src/main/java/my/clinic/myclinic/services/map/VisitMapService.java
@@ -0,0 +1,39 @@
+package my.clinic.myclinic.services.map;
+
+import my.clinic.myclinic.domain.Visit;
+import my.clinic.myclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService  extends  AbstractMapService<Visit,Long> implements VisitService {
+    @Override
+    public Visit findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() ==null || visit.getPet().getOwner() == null || visit.getPet().getId() ==null || visit.getPet().getOwner().getId() == null)
+        {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
