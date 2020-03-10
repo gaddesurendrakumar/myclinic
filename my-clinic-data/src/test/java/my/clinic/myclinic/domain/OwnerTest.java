47f3bd17f33ac8edbfe5a0e66a050ac368792c58
sgadde <gaddesurendrakumar@gmail.com>
2020-03-10 06:46:08 +0530

    Upgraded to JUnit 5, closes #4

---
+++ my-clinic-data/src/test/java/my/clinic/myclinic/domain/OwnerTest.java
@@ -0,0 +1,15 @@
+package my.clinic.myclinic.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OwnerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void setAddress() {
+    }
+}
--- my-clinic-web/src/test/java/my/clinic/myclinic/MyclinicApplicationTests.java
+++ my-clinic-web/src/test/java/my/clinic/myclinic/MyclinicApplicationTests.java
@@ -1,10 +1,13 @@
 package my.clinic.myclinic;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.context.junit4.SpringRunner;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class MyclinicApplicationTests {
     @Test
