26cfe6dd6ff7e26629755dfd06d5d293ffe279d4
sgadde <gaddesurendrakumar@gmail.com>
2020-03-14 01:09:11 +0530

    Added ability to add or Update Visit, closes #57

--- my-clinic-data/src/main/java/my/clinic/myclinic/domain/Pet.java
+++ my-clinic-data/src/main/java/my/clinic/myclinic/domain/Pet.java
@@ -1,6 +1,7 @@
 package my.clinic.myclinic.domain;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -40,6 +41,7 @@ public class Pet  extends  BaseEntity{
 
 
     @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
     @OneToMany(cascade =  CascadeType.ALL, mappedBy = "pet")
     Set<Visit> visits = new HashSet<>();
